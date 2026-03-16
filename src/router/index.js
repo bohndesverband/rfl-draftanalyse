7dd9750caa9f493a748b1f9f32566a6a2b572116
Jakob Eschler <mail@jakob-eschler.de>
2026-03-16 13:38:26 +0100

    changed router history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "@/views/Home.vue";
 import SignUp from "@/views/SignUp.vue";
@@ -17,7 +17,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes,
 	scrollBehavior(to, from, savedPosition) {
 		if (savedPosition) {
